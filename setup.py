dd263c973bcc9d802111a7cc6639fdeb0b2bb58e
dron22 <info@fastback.io>
2016-04-09 14:44:32 -0300

    Added travis/tox/pytest setup

--- setup.py
+++ setup.py
@@ -1,4 +1,35 @@
+
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+class Tox(TestCommand):
+
+    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.tox_args = None
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import tox
+        import shlex
+        args = self.tox_args
+        if args:
+            args = shlex.split(self.tox_args)
+        errno = tox.cmdline(args=args)
+        sys.exit(errno)
 
 setup(
     name='r53dyndns',
@@ -9,9 +40,8 @@ setup(
     author_email='info@fastback.io',
     license='MIT',
     packages=['r53dyndns'],
-    install_requires=[
-        'boto==2.39.0',
-        'requests==2.9.1',
-    ],
+    install_requires=requirements,
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
     zip_safe=False
 )
